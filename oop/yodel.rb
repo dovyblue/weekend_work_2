b39b5c5c86bd3600c39e28f8385bf4a7e4a5245e
Dov Bluming <dovybluming@gmail.com>
2017-03-10 14:03:17 -0500

    Yodel class

--- oop/yodel.rb
+++ oop/yodel.rb
@@ -9,7 +9,15 @@
 # constructor accordingly.
 
 class Yodel
+  attr_reader :segment, :count
+  def initialize(input)
+    @segment = input[:segment]
+    @count = input[:count]
+  end
 
+  def articulate
+    segment * count
+  end
 end
 
 
